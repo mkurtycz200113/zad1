0f1f0d3660591e347afb4ec0c4203076ff0cbaf1
mkurtycz200113 <kurtyczm@gmail.com>
2023-05-20 12:31:15 +0200

    four

--- cw.sh
+++ cw.sh
@@ -1,8 +1,16 @@
 if [ "$1" == "--date" ]; then
   echo $(date)
 elif [ "$1" == "--logs" ]; then
-	for ((i=1; i<=100; i++)); do
-      	echo "log$i.txt" > log$i.txt
-      	echo "Nazwa skryptu: $0" >> log$i.txt
-      	echo "Data: $(date)" >> log$i.txt
-    	done
+  if [ -n "30" ]; then
+    for ((i=1; i<=30; i++)); do
+      echo "log$i.txt" > log$i.txt
+      echo "Nazwa skryptu: $0" >> log$i.txt
+      echo "Data: $(date)" >> log$i.txt
+    done
+  else
+    for ((i=1; i<=100; i++)); do
+      echo "log$i.txt" > log$i.txt
+      echo "Nazwa skryptu: $0" >> log$i.txt
+      echo "Data: $(date)" >> log$i.txt
+    done
+  fi
